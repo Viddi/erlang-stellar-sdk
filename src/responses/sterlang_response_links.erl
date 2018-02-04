5369ee6ac2b58174fe39fc5f06c061362a2fc229
Viðar Ingi Ottósson <vidaringi0217@gmail.com>
2018-02-03 22:43:10 -0800

    Start work on links response

--- src/responses/sterlang_response_links.erl
+++ src/responses/sterlang_response_links.erl
@@ -2,5 +2,27 @@
 
 -behaviour(sterlang_response).
 
--record(sterlang_links, {effects = undefined
-                        }).
+-export([encode/1, decode/1]).
+
+-record(response_links, {effects = undefined :: sterlang_response_effects:response_effects(),
+                         precedes = undefined :: sterlang_response_precedes:response_precedes(),
+                         self = undefined :: sterlang_response_self:response_self(),
+                         succeeds = undefined :: sterlang_response_succeeds:response_succeeds(),
+                         transactions = undefined :: sterlang_response_transactions:response_transactions()}).
+
+-opaque response_links() :: #response_links{}.
+
+-export_type([response_links/0]).
+
+decode(Links) ->
+  Effects = sterlang_response_effects:decode(maps:get(<<"effects">>, Links)),
+  Precedes = sterlang_response_precedes:decode(maps:get(<<"precedes">>, Links)),
+  Self = sterlang_response_self:decode(maps:get(<<"self">>, Links)),
+  Succeeds = sterlang_response_succeeds:decode(maps:get(<<"succeeds">>, Links)),
+  Transactions = sterlang_response_transactions:decode(maps:get(<<"transactions">>, Links)),
+
+  #response_links{effects = Effects, precedes = Precedes, self = Self,
+                 succeeds = Succeeds, transactions = Transactions}.
+
+encode(#response_links{} = Links) ->
+  ok.
